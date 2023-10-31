13a6880cb13b3ac4c1a126ea84af42ff60bed9f9
nghiakma <lam47788@gmail.com>
2023-11-01 00:15:00 +0700

    user controller
    Signed-off-by: nghiakma <lam47788@gmail.com>

---
+++ Backend/controller/userCtrl.js
@@ -0,0 +1,26 @@
+const User = require("../models/userModel");
+const uniqid = require("uniqid");
+
+const createUser = asyncHandler(async (req, res) => {
+    /**
+     * TODO:Get the email from req.body
+     */
+    const email = req.body.email;
+    /**
+     * TODO:With the help of email find the user exists or not
+     */
+    const findUser = await User.findOne({ email: email });
+  
+    if (!findUser) {
+      /**
+       * TODO:if user not found user create a new user
+       */
+      const newUser = await User.create(req.body);
+      res.json(newUser);
+    } else {
+      /**
+       * TODO:if user found then thow an error: User already exists
+       */
+      throw new Error("User Already Exists");
+    }
+  });
