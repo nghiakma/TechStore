a8c86889f3231246cc2518156ffabdb32318dbf6
nghiakma <lam47788@gmail.com>
2024-01-26 00:16:00 +0700

    bstLogin
    Signed-off-by: nghiakma <lam47788@gmail.com>

--- Admin/my-app/src/App.js
+++ Admin/my-app/src/App.js
@@ -1,7 +1,8 @@
 import "./App.css";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Login from "./pages/Login";
-
+import { OpenRoutes } from "./routing/OpenRoutes";
+import { PrivateRoutes } from "./routing/PrivateRoutes";
 
 function App() {
   return (
