9650575b8ed79a7d650268025608d8a44cfd3090
nghiakma <lam47788@gmail.com>
2024-01-26 00:16:00 +0700

    redux login
    Signed-off-by: nghiakma <lam47788@gmail.com>

---
+++ Admin/my-app/src/features/auth/authServices.js
@@ -0,0 +1,21 @@
+import axios from "axios";
+import { config } from "../../utils/axiosconfig";
+import { base_url } from "../../utils/baseUrl";
+
+// const getTokenFromLocalStorage = localStorage.get("user")
+//   ? JSON.parse(localStorage.getItem("user"))
+//   : null;
+
+const login = async (user) => {
+  const response = await axios.post(`${base_url}user/admin-login`, user);
+  if (response.data) {
+    localStorage.setItem("user", JSON.stringify(response.data));
+  }
+  return response.data;
+};
+
+const authService = {
+    login,
+};
+  
+export default authService;
