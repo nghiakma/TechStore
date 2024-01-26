a989cc3716e71411e0c5c7d8296dcf916be26201
nghiakma <lam47788@gmail.com>
2024-01-27 00:22:00 +0700

    Brand
    Signed-off-by: nghiakma <lam47788@gmail.com>

--- Admin/my-app/src/App.js
+++ Admin/my-app/src/App.js
@@ -5,6 +5,10 @@ import { OpenRoutes } from "./routing/OpenRoutes";
 import { PrivateRoutes } from "./routing/PrivateRoutes";
 import MainLayout from "./components/MainLayout";
 import Customers from "./pages/Customers";
+import Addcat from "./pages/Addcat";
+import Categorylist from "./pages/Categorylist";
+import Brandlist from "./pages/Brandlist";
+import Addbrand from "./pages/Addbrand";
 function App() {
   return (
     <Router>
@@ -29,6 +33,12 @@ function App() {
         >
          
          <Route path="customers" element={<Customers />} />
+         <Route path="list-brand" element={<Brandlist />} />
+          <Route path="brand" element={<Addbrand />} />
+          <Route path="brand/:id" element={<Addbrand />} />
+         <Route path="list-category" element={<Categorylist />} />
+         <Route path="category" element={<Addcat />} />
+         <Route path="category/:id" element={<Addcat />} />
         </Route>
       </Routes>
     </Router>
--- Admin/my-app/src/app/store.js
+++ Admin/my-app/src/app/store.js
@@ -3,12 +3,13 @@ import authReducer from "../features/auth/authSlice";
 import customerReducer from "../features/cutomers/customerSlice";
 import pCategoryReducer from "../features/pcategory/pcategorySlice";
 import uploadReducer from "../features/upload/uploadSlice";
-
+import brandReducer from "../features/brand/brandSlice";
 export const store = configureStore({
   reducer: {
     auth: authReducer,
     customer: customerReducer,
     pCategoryReducer,
-    uploadReducer
+    uploadReducer,
+    brand: brandReducer,
   },
 });
