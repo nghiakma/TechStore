ea83e2ab3c44a4281c0dec31d6c02b186f890cf7
nghiakma <lam47788@gmail.com>
2024-03-02 01:22:00 +0700

    ezLayout
    Signed-off-by: nghiakma <lam47788@gmail.com>

---
+++ Fronend/my-app/src/components/Footer.js
@@ -0,0 +1,10 @@
+const Footer = () => {
+    return (
+      <header>
+         <h1>đádasdasd</h1>
+      </header>
+    );
+  }
+  
+  export default Footer;
+  
---
+++ Fronend/my-app/src/components/Header.js
@@ -0,0 +1,9 @@
+const Header = () => {
+  return (
+    <header>
+       <h1>đádasdasd</h1>
+    </header>
+  );
+}
+
+export default Header;
