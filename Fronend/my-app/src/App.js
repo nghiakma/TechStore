4cd1796fa574656639a23359d392c804d48572c6
nghiakma <lam47788@gmail.com>
2024-03-07 01:22:00 +0700

    policy&br
    Signed-off-by: nghiakma <lam47788@gmail.com>

--- Fronend/my-app/src/App.js
+++ Fronend/my-app/src/App.js
@@ -3,7 +3,11 @@ import "./App.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import Layout from "./components/Layout";
 import Home from "./pages/Home";
-
+import About from "./pages/About";
+import PrivacyPolicy from "./pages/PrivacyPolicy";
+import RefundPloicy from "./pages/RefundPloicy";
+import ShippingPolicy from "./pages/ShippingPolicy";
+import TermAndContions from "./pages/TermAndContions";
 function App() {
   return (
     <>
@@ -11,6 +15,12 @@ function App() {
         <Routes>
           <Route path="/" element={<Layout />}>
            <Route index element={<Home />} />
+           <Route path="about" element={<About />} />
+
+           <Route path="privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="refund-policy" element={<RefundPloicy />} />
+            <Route path="shipping-policy" element={<ShippingPolicy />} />
+            <Route path="term-conditions" element={<TermAndContions />} />
           </Route>
         </Routes>
       </BrowserRouter>
---
+++ Fronend/my-app/src/pages/Contact.js
@@ -0,0 +1,161 @@
+import React from "react";
+import BreadCrumb from "../components/BreadCrumb";
+import Meta from "../components/Meta";
+import { AiOutlineHome, AiOutlineMail } from "react-icons/ai";
+import { BiPhoneCall, BiInfoCircle } from "react-icons/bi";
+import Container from "../components/Container";
+import { useFormik } from "formik";
+import * as yup from "yup";
+import { useDispatch } from "react-redux";
+import { createQuery } from "../features/contact/contactSlice";
+
+let contactSchema = yup.object({
+  name: yup.string().required("Tên không được để trống"),
+  email: yup
+    .string()
+    .required("Email không được để trống")
+    .email("Email phải hợp lệ"),
+  mobile: yup.number().required().positive().integer("Sđt khong được để trống"),
+  comment: yup.string().required("Bình luận không được để trống"),
+});
+
+const Contact = () => {
+  const dispatch = useDispatch();
+  const formik = useFormik({
+    initialValues: {
+      name: "",
+      email: "",
+      mobile: "",
+      comment: "",
+    },
+    validationSchema: contactSchema,
+    onSubmit: (values) => {
+      dispatch(createQuery(values));
+    },
+  });
+  return (
+    <>
+      <Meta title={"Liên hệ với chúng tôi"} />
+      <BreadCrumb title="Liên hệ với chúng tôi" />
+      <Container class1="contact-wrapper py-5 home-wrapper-2">
+        <div className="row">
+          <div className="col-12">
+            <iframe
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d6986.771103663534!2d76.99275607711007!3d28.886888929272477!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x390da5e51463d4c9%3A0xe5a485e2ac7c3d4a!2sMandaura%2C%20Haryana%20131103!5e0!3m2!1sen!2sin!4v1669909087902!5m2!1sen!2sin"
+              width="600"
+              height="450"
+              className="border-0 w-100"
+              allowFullScreen=""
+              loading="lazy"
+              referrerPolicy="no-referrer-when-downgrade"
+            ></iframe>
+          </div>
+          <div className="col-12 mt-5">
+            <div className="contact-inner-wrapper d-flex justify-content-between ">
+              <div>
+                <h3 className="contact-title mb-4">Liên hệ</h3>
+                <form
+                  action=""
+                  onSubmit={formik.handleSubmit}
+                  className="d-flex flex-column gap-15"
+                >
+                  <div>
+                    <input
+                      type="text"
+                      className="form-control"
+                      placeholder="Tên"
+                      name="name"
+                      onChange={formik.handleChange("name")}
+                      onBlur={formik.handleBlur("name")}
+                      value={formik.values.name}
+                    />
+                    <div className="error">
+                      {formik.touched.name && formik.errors.name}
+                    </div>
+                  </div>
+
+                  <div>
+                    <input
+                      type="email"
+                      className="form-control"
+                      placeholder="Email"
+                      name="email"
+                      onChange={formik.handleChange("email")}
+                      onBlur={formik.handleBlur("email")}
+                      value={formik.values.email}
+                    />
+                    <div className="error">
+                      {formik.touched.email && formik.errors.email}
+                    </div>
+                  </div>
+                  <div>
+                    <input
+                      type="tel"
+                      className="form-control"
+                      placeholder="SĐT"
+                      name="mobile"
+                      onChange={formik.handleChange("mobile")}
+                      onBlur={formik.handleBlur("mobile")}
+                      value={formik.values.mobile}
+                    />
+                    <div className="error">
+                      {formik.touched.mobile && formik.errors.mobile}
+                    </div>
+                  </div>
+                  <div>
+                    <textarea
+                      id=""
+                      className="w-100 form-control"
+                      cols="30"
+                      rows="4"
+                      placeholder="Bình luận"
+                      name="comment"
+                      onChange={formik.handleChange("comment")}
+                      onBlur={formik.handleBlur("comment")}
+                      value={formik.values.comment}
+                    ></textarea>
+                    <div className="error">
+                      {formik.touched.comment && formik.errors.comment}
+                    </div>
+                  </div>
+                  <div>
+                    <button className="button border-0">Gửi</button>
+                  </div>
+                </form>
+              </div>
+              <div>
+                <h3 className="contact-title mb-4">Liên hệ với chúng tôi</h3>
+                <div>
+                  <ul className="ps-0">
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineHome className="fs-5" />
+                      <address className="mb-0">
+                        Ha dong, Ha Noi
+                      </address>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiPhoneCall className="fs-5" />
+                      <a href="tel:+91 8264954234">+84 843016069</a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <AiOutlineMail className="fs-5" />
+                      <a href="mailto:devjariwala8444@gmail.com">
+                        nghiangua111@gmail.com
+                      </a>
+                    </li>
+                    <li className="mb-3 d-flex gap-15 align-items-center">
+                      <BiInfoCircle className="fs-5" />
+                      <p className="mb-0">Thứ hai – Thứ 6 10 sáng – 8 tối</p>
+                    </li>
+                  </ul>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </Container>
+    </>
+  );
+};
+
+export default Contact;
